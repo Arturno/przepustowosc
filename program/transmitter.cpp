5db3d7395f6eae3f2a0cf463e875f9643a1964b8
Arturno <37472216+Arturno@users.noreply.github.com>
2019-03-05 09:57:05 +0100

    Update transmitter.cpp

--- program/transmitter.cpp
+++ program/transmitter.cpp
@@ -8,10 +8,14 @@
 #include "sterowanieTX.cpp"
 #include "ControlTX.cpp"
 #include "Adaptacja.cpp"
-
 #include <thread>
+
 using namespace std;
+
+//Częśc służąca nadawaniu pakietów
+
 void pomiar(int &stan, int &licznik, int &grupa_pakietow, int rozmiar)
+//Funkcja dodatkowa mierząca liczbę wysłanych pakietów w ostatniej sekundzie
 {
     int ostatnio = 0;
     int wyslane = 0;
@@ -33,7 +37,9 @@ void pomiar(int &stan, int &licznik, int &grupa_pakietow, int rozmiar)
 }
 
 void transmitter()
+//Główna funkcja części nadawczej
 {
+    //Tworzenie struktur zawierających adresy strony nadawczej i odbiorczej
     struct sockaddr_in TX_TCP = {
         .sin_family = AF_INET,
         .sin_port = htons(8888)};
@@ -44,8 +50,9 @@ void transmitter()
     struct sockaddr_in RX = {};
 
     cout << "TRYB TRANSMITER" << endl;
+    //Oczekiwanie na połączenie przychodzące
     cout << "Oczekiwanie na przyłączenie RX" << endl;
-    //czesc TCP
+    //Zestawianie połączenia TCP w celu bezbłędnej wymiany parametrów transmisji
     if (inet_pton(AF_INET, "127.0.0.1", &TX_TCP.sin_addr) <= 0)
     {
         perror("inet_pton() ERROR");
@@ -98,7 +105,8 @@ void transmitter()
     char buffer_ip[128] = {};
     cout << "IP RX:  ..........  " << inet_ntop(AF_INET, &RX.sin_addr, buffer_ip, sizeof(buffer_ip)) << endl;
     cout << "port:  ...........  " << ntohs(RX.sin_port) << endl;
-
+    
+    //Utworzenie struktury zawierającej paraketry transmisji
     class OgolneInfo parametry(buffer);
     parametry.wypisanie();
     class SterowanieTX ster(parametry.przeplywnosc, parametry.rozmiar_pakietu);
@@ -108,7 +116,7 @@ void transmitter()
     cout<<"M xxx - zmiana przepływności na wartość xxx Mb/s"<<endl;
     cout<<"E - zakonczenie pomiaru"<<endl;
 
-
+        
         thread sterowanieProgramem(ControlTX, ref(ster.stan), ref(ster), RXSocket);
         thread adapt(adaptacja, ref(ster.licznik), ref(ster.do_wyslania), ref(ster.grupa_pakietow), ref(ster.opoznienie), ref(ster.stan), parametry.czas_adaptacji);
         //thread pomiaaar(pomiar, ref(ster.stan), ref(ster.licznik), ref(ster.grupa_pakietow), ster.rozmiar_pakietu);
